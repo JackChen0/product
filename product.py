b9a67969d6b7fe5e5d126396c0f73a36438917b2
JackChen0 <stu30305@gmail.com>
2019-01-07 00:37:35 +0800

    Add header

--- product.py
+++ product.py
@@ -5,9 +5,15 @@ while True:
 	if product == 'end':
 		break
 	price = input ('Enter the price of product. ')
+	price = int(price)
 	pr.append([product,price])
 	
 
 print(pr)
 for p in pr:
-	print('The price of ',p[0],'is', p[1])
+	print('The price of ',p[0],'is', str(p[1]))
+
+with open('product.csv', 'w') as f:
+	f.write('name, price\n')
+	for p in pr:
+		f.write(p[0] + ','+ str(p[1]) + '\n')
